8f64df3d0da40e5d6fa5d69a419242143bf0c2ff
Jeremias Ceciliano <jceciliano@soin.co.cr>
2015-04-24 17:30:55 -0600

    separating group with group details

--- Group/models.py
+++ Group/models.py
@@ -5,9 +5,19 @@ from Tournement.models import Tournement
 
 # Create your models here.
 class Group(models.Model):
-	player = models.ForeignKey(Player)
-	tournement = models.ForeignKey(Tournement)
 	code = models.CharField(max_length=50)
+	tournement = models.ForeignKey(Tournement)
+
+	class Meta:
+		verbose_name = 'Group'
+		verbose_name_plural = 'Groups'
+
+	def __unicode__(self):
+		return 'Group:'+self.code
+
+class GroupDetail(models.Model):
+	group = models.ForeignKey(Group)
+	player = models.ForeignKey(Player)
 	points = models.DecimalField(max_digits=18,decimal_places=0)
 	games_played = models.DecimalField(max_digits=18,decimal_places=0)
 	wins = models.DecimalField(max_digits=18,decimal_places=0)
@@ -20,11 +30,8 @@ class Group(models.Model):
 	sets_looses = models.DecimalField(max_digits=18,decimal_places=0)
 
 	class Meta:
-		verbose_name = 'Group'
-		verbose_name_plural = 'Groups'
+		verbose_name = 'Group Detail'
+		verbose_name_plural = 'Group Details'
 
 	def __unicode__(self):
-		return 'Group:'+self.code
-
-#class GroupDetail(models.Model):
-	#
+		return 'Group:'+self.group.code+',player:'+self.player.player_name
