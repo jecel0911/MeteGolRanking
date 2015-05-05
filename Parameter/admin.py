1194c2038d20a744e8ea09dad223118b5a3b1e15
jeremias <jecel0911@gmail.com>
2015-05-05 16:58:17 -0600

    changing labels

--- Parameter/admin.py
+++ Parameter/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 
 class ParameterAdmin(admin.ModelAdmin):
-	list_display = ('players_by_division','points_earned_when_winner_is_higher',
-		            'points_earned_when_winner_is_lower','points_earned_when_loser_is_higher',
-		            'points_earned_when_loser_is_lower')
+	list_display = ('players_by_division','earned_points_when_winner_is_higher',
+		            'earned_points_when_winner_is_lower','lost_points_when_loser_is_higher',
+		            'lost_points_when_loser_is_lower')
 	
 	def has_add_permission(self, request, obj=None):
 		if Parameter.objects.all().count() == 0:
--- Parameter/models.py
+++ Parameter/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.db import models
+from django.db import models 
 
 class Parameter(models.Model):
 	players_by_division = models.DecimalField(max_digits=18,decimal_places=0)
@@ -7,10 +7,10 @@ class Parameter(models.Model):
 	bonus_points_2_place = models.DecimalField(max_digits=18,decimal_places=0)
 	bonus_points_3_place = models.DecimalField(max_digits=18,decimal_places=0)
 	bonus_points_4_place = models.DecimalField(max_digits=18,decimal_places=0)
-	points_earned_when_winner_is_higher = models.DecimalField(max_digits=18,decimal_places=0)
-	points_earned_when_winner_is_lower = models.DecimalField(max_digits=18,decimal_places=0)
-	points_earned_when_loser_is_higher = models.DecimalField(max_digits=18,decimal_places=0)
-	points_earned_when_loser_is_lower = models.DecimalField(max_digits=18,decimal_places=0)
+	earned_points_when_winner_is_higher = models.DecimalField(max_digits=18,decimal_places=0)
+	earned_points_when_winner_is_lower = models.DecimalField(max_digits=18,decimal_places=0)
+	lost_points_when_loser_is_higher = models.DecimalField(max_digits=18,decimal_places=0)
+	lost_points_when_loser_is_lower = models.DecimalField(max_digits=18,decimal_places=0)
 
 	class Meta:
 		verbose_name = 'Parameter'
