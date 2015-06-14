90d702c63023b57a0be7a306fe340974f7c8b276
Bernal <bernalmora@gmail.com>
2015-06-14 15:23:57 -0600

    se agregan el pintado de prueba del ranking

--- Public/views.py
+++ Public/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import get_object_or_404,render
 from django.views import generic
 
+from Rankings.models import Ranking
 
 def index(request):
+	lista_jugadores = Ranking.objects.order_by('-puntos')
+	context = {'lista_jugadores': lista_jugadores}
 	template_name = 'Public/Home.html'
-	return render(request,template_name)
+	return render(request,template_name,context)
+
 
